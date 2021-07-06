6908cbe7a22370c571e10d8d61d29c1a664ffaa2
Volodymyr Rusynov <vrusynov@gmail.com>
2021-07-07 01:18:37 +0300

    feat: server

--- workspaces/server/index.ts
+++ workspaces/server/index.ts
@@ -1,2 +1,17 @@
-// server
-('');
+import fastify from 'fastify';
+
+const app = fastify({ logger: true });
+
+app.get('/', function (request, reply) {
+  request;
+  reply.send({ hello: 'world' });
+});
+
+app.listen(3000, function (err, address) {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+
+  app.log.info(`server listening on ${address}`);
+});
